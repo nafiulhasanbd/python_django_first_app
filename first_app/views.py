4c19b737d01a4f6e2a33ebc1fc7aefce7013b28b
nafiulhasanbd <nafiulhasanbd@gmail.com>
2018-02-26 03:30:36 +0600

    created Model and other few updates

--- first_app/views.py
+++ first_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from first_app.models import Topic, Webpage, AccessRecord
 # Create your views here.
 
 
 def index(request):
-    my_dict = {'insert_me': 'Hello I am from Views.py'}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'Access_records': webpages_list}
+    #my_dict = {'insert_me': 'Hello I am from Views.py'}
+    return render(request, 'first_app/index.html', context=date_dict)
--- first_project/settings.py
+++ first_project/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    STATIC_DIR
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles)
